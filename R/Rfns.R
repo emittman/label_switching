54e0d8a3bf6e688d9a5ce871d72f97e4a2c73d92
emittman <emittman@iastate.edu>
2017-03-28 12:31:42 -0500

    Add line to .R file specifying dynamic library use

--- R/Rfns.R
+++ R/Rfns.R
@@ -1,6 +1,7 @@
 #' @title sample_wwr
 #' @description Weighted sampling without replacement
 #' @param weights numeric vector of weights
+#' @useDynLib labelswitching
 #' @export
 sample_wwr <- function(weights){
   .Call("Rsample_wwr", sample(1e6, 1), as.numeric(weights))

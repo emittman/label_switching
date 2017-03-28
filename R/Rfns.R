8032027622360fed48893929f649258dc86d21fc
emittman <emittman@iastate.edu>
2017-03-28 12:36:55 -0500

    Move library linkage to separate file

--- R/Rfns.R
+++ R/Rfns.R
@@ -1,7 +1,6 @@
 #' @title sample_wwr
 #' @description Weighted sampling without replacement
 #' @param weights numeric vector of weights
-#' @useDynLib labelswitching
 #' @export
 sample_wwr <- function(weights){
   .Call("Rsample_wwr", sample(1e6, 1), as.numeric(weights))
---
+++ R/namespace.R
@@ -0,0 +1,2 @@
+#' @useDynLib labelswitching
+NULL

3e43a135beb56a0963fcccd68e6252c26cb59f54
emittman <emittman@iastate.edu>
2017-03-28 12:13:24 -0500

    Try to get roxygen to explicitly export

--- R/Rfns.R
+++ R/Rfns.R
@@ -1,6 +1,6 @@
 #' @title sample_wwr
 #' @description Weighted sampling without replacement
-#' @param weights
+#' @param weights numeric vector of weights
 
 sample_wwr <- function(weights){
   .Call("Rsample_wwr", sample(1e6, 1), as.numeric(weights))
